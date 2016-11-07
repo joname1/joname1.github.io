71881c3995485fbecf954c264fd14e0e7a7f7e0a
jonames <gyjhkx57164@027168.com>
2016-11-07 10:08:20 +0000

    Site updated: 2016-11-07 18:08:16

---
+++ js/totop.js
@@ -0,0 +1,1 @@
+!function(o){var t=1e3,n=o("#totop"),c=1600;n.hide(),o(window).scroll(function(){var c=o(document).scrollTop();c>t?o(n).stop().fadeTo(300,1):o(n).stop().fadeTo(300,0)}),o(n).click(function(){return o("html, body").animate({scrollTop:0},c),!1})}(jQuery);
