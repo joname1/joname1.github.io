2312ba9edf3b89d45743a660b310c4154fd9d1cf
jonames <gyjhkx57164@027168.com>
2016-11-08 05:37:30 +0000

    Site updated: 2016-11-08 13:37:28

--- js/main.js
+++ js/main.js
@@ -1,1 +1,94 @@
-require([],function(){function i(){$(".article").each(function(){$(this).offset().top<=$(window).scrollTop()+$(window).height()&&!$(this).hasClass("show")?($(this).removeClass("hidden").addClass("show"),$(this).addClass("is-hiddened")):$(this).hasClass("is-hiddened")||$(this).addClass("hidden")})}var n=!1,e=function(){require(["/js/mobile.js"],function(i){i.init(),n=!0})},a=!1,t=function(){require(["/js/pc.js"],function(i){i.init(),a=!0})},r={versions:function(){var i=window.navigator.userAgent;return{trident:i.indexOf("Trident")>-1,presto:i.indexOf("Presto")>-1,webKit:i.indexOf("AppleWebKit")>-1,gecko:i.indexOf("Gecko")>-1&&i.indexOf("KHTML")==-1,mobile:!!i.match(/AppleWebKit.*Mobile.*/),ios:!!i.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),android:i.indexOf("Android")>-1||i.indexOf("Linux")>-1,iPhone:i.indexOf("iPhone")>-1||i.indexOf("Mac")>-1,iPad:i.indexOf("iPad")>-1,webApp:i.indexOf("Safari")==-1,weixin:i.indexOf("MicroMessenger")==-1}}()};$(window).bind("resize",function(){if(n&&a)return void $(window).unbind("resize");var i=$(window).width();i>=700?t():e()}),r.versions.mobile===!0||$(window).width()<700?e():t(),yiliaConfig.fancybox===!0&&require(["/fancybox/jquery.fancybox.js"],function(i){var n=$(".isFancy");if(0!=n.length){for(var e=$(".article-inner img"),a=0,t=e.length;a<t;a++){var r=e.eq(a).attr("src"),o=e.eq(a).attr("alt");e.eq(a).replaceWith("<a href='"+r+"' title='"+o+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+r+"' title='"+o+"'></a>")}$(".article-inner .fancy-ctn").fancybox()}}),yiliaConfig.animate===!0&&(require(["/js/jquery.lazyload.js"],function(){$(".js-avatar").attr("src",$(".js-avatar").attr("lazy-src")),$(".js-avatar")[0].onload=function(){$(".js-avatar").addClass("show")}}),yiliaConfig.isHome===!0&&($(window).on("scroll",function(){i()}),i())),1==yiliaConfig.open_in_new&&$(".article a[href]").attr("target","_blank")});
+require([], function() {
+	var isMobileInit = false;
+	var loadMobile = function() {
+		require(["/js/mobile.js"], function(mobile) {
+			mobile.init();
+			isMobileInit = true
+		})
+	};
+	var isPCInit = false;
+	var loadPC = function() {
+		require(["/js/pc.js"], function(pc) {
+			pc.init();
+			isPCInit = true
+		})
+	};
+	var browser = {
+		versions: function() {
+			var u = window.navigator.userAgent;
+			return {
+				trident: u.indexOf("Trident") > -1,
+				presto: u.indexOf("Presto") > -1,
+				webKit: u.indexOf("AppleWebKit") > -1,
+				gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1,
+				mobile: !!u.match(/AppleWebKit.*Mobile.*/),
+				ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+				android: u.indexOf("Android") > -1 || u.indexOf("Linux") > -1,
+				iPhone: u.indexOf("iPhone") > -1 || u.indexOf("Mac") > -1,
+				iPad: u.indexOf("iPad") > -1,
+				webApp: u.indexOf("Safari") == -1,
+				weixin: u.indexOf("MicroMessenger") == -1
+			}
+		}()
+	};
+	$(window).bind("resize", function() {
+		if (isMobileInit && isPCInit) {
+			$(window).unbind("resize");
+			return
+		}
+		var w = $(window).width();
+		if (w >= 700) {
+			loadPC()
+		} else {
+			loadMobile()
+		}
+	});
+	if (browser.versions.mobile === true || $(window).width() < 700) {
+		loadMobile()
+	} else {
+		loadPC()
+	}
+	if (yiliaConfig.fancybox === true) {
+		require(["/fancybox/jquery.fancybox.js"], function(pc) {
+			var isFancy = $(".isFancy");
+			if (isFancy.length != 0) {
+				var imgArr = $(".article-inner img");
+				for (var i = 0, len = imgArr.length; i < len; i++) {
+					var src = imgArr.eq(i).attr("src");
+					var title = imgArr.eq(i).attr("alt");
+					imgArr.eq(i).replaceWith("<a href='" + src + "' title='" + title + "' rel='fancy-group' class='fancy-ctn fancybox'><img src='" + src + "' title='" + title + "'></a>")
+				}
+				$(".article-inner .fancy-ctn").fancybox()
+			}
+		})
+	}
+	if (yiliaConfig.animate === true) {
+		require(["/js/jquery.lazyload.js"], function() {
+			$(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
+			$(".js-avatar")[0].onload = function() {
+				$(".js-avatar").addClass("show")
+			}
+		});
+		if (yiliaConfig.isHome === true) {
+			function showArticle() {
+				$(".article").each(function() {
+					if ($(this).offset().top <= $(window).scrollTop() + $(window).height() && !($(this).hasClass("show"))) {
+						$(this).removeClass("hidden").addClass("show");
+						$(this).addClass("is-hiddened")
+					} else {
+						if (!$(this).hasClass("is-hiddened")) {
+							$(this).addClass("hidden")
+						}
+					}
+				})
+			}
+			$(window).on("scroll", function() {
+				showArticle()
+			});
+			showArticle()
+		}
+	}
+	if (yiliaConfig.open_in_new == true) {
+		$(".article a[href]").attr("target", "_blank")
+	}
+});
